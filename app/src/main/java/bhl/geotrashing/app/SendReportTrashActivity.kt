21207f5bfb067fbbd798651c6ee88b92e6533b20
h4dri <adrianmalik333@gmail.com>
2020-11-06 21:05:59 +0100

    Send Form

--- app/src/main/java/bhl/geotrashing/app/SendReportTrashActivity.kt
+++ app/src/main/java/bhl/geotrashing/app/SendReportTrashActivity.kt
@@ -1,12 +1,11 @@
 package bhl.geotrashing.app
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import bhl.geotrashing.app.firestore.DataBase
 import kotlinx.android.synthetic.main.activity_send_report_trash.*
-import java.io.FileInputStream
 
 
 class SendReportTrashActivity : AppCompatActivity() {
@@ -16,6 +15,12 @@ class SendReportTrashActivity : AppCompatActivity() {
         val intent : Intent = getIntent()
         val takenImage = intent.getStringExtra("path")
         val decodedTakenImage = BitmapFactory.decodeFile(takenImage)
-        activity_send_report_trash_imageView.setImageBitmap(decodedTakenImage)
+        val db = DataBase()
+        activity_send_report_trash_imageViewId.setImageBitmap(decodedTakenImage)
+
+        activity_send_report_trash_btnSendId.setOnClickListener {
+            db.uploadBitmapa(decodedTakenImage,"z Palca")
+        }
     }
+
 }
