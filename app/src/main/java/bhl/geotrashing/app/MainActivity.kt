7993dae122484249cec4e8a3770a236958e9247f
Piotr <zdun.piotr@gmail.com>
2020-11-06 17:32:25 +0100

    Added Navigation between activities

--- app/src/main/java/bhl/geotrashing/app/MainActivity.kt
+++ app/src/main/java/bhl/geotrashing/app/MainActivity.kt
@@ -1,11 +1,44 @@
 package bhl.geotrashing.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cleanTrashIntent: Intent
+    lateinit var rankingIntent: Intent
+    lateinit var reportTrashIntent: Intent
+    lateinit var verifyTrashIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cleanTrashIntent = Intent(this, CleanTrashActivity::class.java)
+        rankingIntent = Intent(this, RankingActivity::class.java)
+        reportTrashIntent = Intent(this, ReportTrashActivity::class .java)
+        verifyTrashIntent = Intent(this, VerifyTrashActivity::class.java)
+    }
+
+    fun startCleanTrashActivity(view: View)
+    {
+        startActivity(this.cleanTrashIntent)
+    }
+
+    fun startRankingActivity(view: View)
+    {
+        startActivity(this.rankingIntent)
+    }
+
+    fun startReportTrashActivity(view: View)
+    {
+        startActivity(this.reportTrashIntent)
+    }
+
+    fun startVerifyTrashActivity(view: View)
+    {
+        startActivity(this.verifyTrashIntent)
     }
 }
