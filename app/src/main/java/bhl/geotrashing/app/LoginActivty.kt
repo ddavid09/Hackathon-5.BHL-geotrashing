f2794e8e0e0cd4f948d78f97c194f5245bdd9d90
Kajkitsu <norbert.waszkowiak@gmail.com>
2020-11-07 03:39:40 +0100

    add nickname

--- app/src/main/java/bhl/geotrashing/app/LoginActivty.kt
+++ app/src/main/java/bhl/geotrashing/app/LoginActivty.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import bhl.geotrashing.app.firestore.DataBase
 import com.google.firebase.auth.FirebaseAuth
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -54,6 +55,9 @@ class LoginActivty : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
+                val email = user!!.email
+                val nickname = email!!.substring(0,email.indexOf('@')-1)
+                DataBase(this).setUserNickname(nickname)
                 startActivity(this.mainMenuIntent)
                 // ...
             } else {
